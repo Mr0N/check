82815d0cf23cb4f915a78bc3a8f80ea6cb3ec1a2
new_user <new_user@gmail.com>
2021-06-09 22:08:55 +0300

    update

--- Check3333/Program.cs
+++ Check3333/Program.cs
@@ -1,27 +1,63 @@
 ﻿using SplitArray;
 using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Check3333
 {
+    class XXX
+    {
+        public static int Sum(int one, int two)
+        {
+            Thread.Sleep(1);
+            return 0;
+        }
+        public int SumNotStatic(int one, int two)
+        {
+            Thread.Sleep(1);
+            return 0;
+        }
+    }
+   static class InfoStatic
+    {
+
+  
+
+        public static void Start()
+        {
+            var result = Enumerable.Range(0, 500000)
+                 .Split(30)
+                 .Select(a => Task.Run(() =>
+                 {
+                     Thread.Sleep(1000);
+                     return a.Select(z => XXX.Sum(z, 2)).ToList();
+                 }))
+                 .ToArray();
+            Task.WaitAll(result);
+        }
+    }
     class Info
     {
-        static int Sum(int one,int two)
-        {
-            Console.Write(Thread.CurrentThread.ManagedThreadId);
-            var info = one + two;
-            Console.Write(Thread.CurrentThread.ManagedThreadId);
-            return info;
-        }
+
+
         public void Start()
         {
-           var result =  Enumerable.Range(0, 5000)
-                .Split(30)
-                .Select(a => Task.Run(() => a.Select(z => Sum(z, 2)).ToList()))
-                .ToArray();
+            var result = Enumerable.Range(0, 5000)
+                 .Split(30)
+                 .Select(a => Task.Run(() =>
+                 {
+                     Thread.Sleep(1000);
+                     var sum = new XXX();
+                     return a.Select(z => sum.SumNotStatic(z, 2)).ToList();
+                 }))
+                 .ToArray();
             Task.WaitAll(result);
+        }
+        public Info()
+        {
+
         }
     }
     class Program
@@ -29,7 +65,19 @@ namespace Check3333
         static void Main(string[] args)
         {
             new Info().Start();
-            Console.ReadKey();  
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            new Info().Start();
+            stopwatch.Stop();
+            Console.WriteLine("Not Static " + stopwatch.ElapsedMilliseconds);
+            stopwatch.Reset();
+
+            Console.WriteLine("Static");
+            stopwatch.Start();
+            InfoStatic.Start();
+            stopwatch.Stop();
+            Console.WriteLine("Static " + stopwatch.ElapsedMilliseconds);
+            Console.ReadKey();
         }
     }
 }
